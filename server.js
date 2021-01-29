93d6d33eef092643f01250b40f1d552898724b1a
Chika Ejiofor <chika ejiofor>
2021-01-29 16:04:47 +0100

    'ok'\

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+// call in express
 const app = express();
+
+//connect to Database
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Hello World' }));
 
+// instantiate API routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
+// Create Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
